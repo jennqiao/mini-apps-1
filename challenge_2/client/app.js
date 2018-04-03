43414c99d855844fcf7580c53e8117c09454c88f
jennqiao <jennifercqiao@gmail.com>
2018-04-03 12:34:03 -0700

    csv conversion works

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -3,7 +3,25 @@ $(document).ready(function() {
 
   $(document).on('click', '#submitButton', function() {
     var text = $('input[type=text]').val();
-    
+    var textObj = {'result': text};
+    console.log(textObj);
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://127.0.0.1:3000',
+      data: JSON.stringify(textObj), 
+      contentType: 'application/json',
+      // dataType: 'application/json',
+      success: function(data, status) {
+        console.log('received data!', data);
+        console.log(status)
+
+        //display data on screen
+      }
+    })
+
+
+
   })
 
 });
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -24,6 +24,70 @@ app.use(function(req, res, next) {
 
 app.use(express.static('client'));
 
+app.post('/', function(req, res) {
+
+  // console.log('here is the req', req);
+  console.log('got the req body!', req.body);
+
+  handleReq(req.body.result, function(csv) {
+      res.send(JSON.stringify(csv));
+  });
+
+})
+
+function handleReq(data, callback) {
+
+  //flatten obj with nested children objs
+  //join array with commas
+
+  //loop through obj keys and push into array
+    //skip children key
+  //for each obj
+    //loop through key values, and push to array
+    //if it has children,
+      //for each children, do the same thing
+
+  var headers = [];
+  var content = [];
+
+  for (var key in data) {
+    if (key !== 'children') {
+      headers.push(key);
+    }
+  }
+
+  function addToCSV(obj) {
+
+    var row = [];
+
+    for (var i=0; i<headers.length; i++) {
+      row.push(obj[headers[i]]);
+    }
+
+    content.push(row);
+
+    if (obj['children']) {
+
+      for (var i=0; i<obj['children'].length; i++) {
+        addToCSV(obj['children'][i]);
+      }
+    }
+  }
+
+  addToCSV(data);
+
+
+  var string = headers.join(',') + '\n';
+  for (var i=0; i<content.length; i++) {
+    content[i] = content[i].join(',');
+  }
+  string += content.join('\n');
+
+
+  return string;
+
+
+}
 
 
 
